966ff216aa22f99146d5ed7d93577e3536febaa7
asarfraz <adeel.sarfraz@gmail.com>
2016-03-13 12:11:11 +0500

    Added files via upload

---
+++ gen_transparent.php
@@ -0,0 +1,9 @@
+<?php
+$image = imagecreatetruecolor(700, 300);
+imagealphablending($image, true);
+imagesavealpha($image, true);
+$text_color = imagecolorallocate($image, 255, 0, 0);
+$bgcolor = imagecolorallocatealpha($image, 0, 0, 0, 127);
+imagefill($image, 0, 0, $bgcolor);
+imagestring($image,2, 100, 150, 'Text written on transparent background', $text_color);
+imagepng($image,'transparent.png');
